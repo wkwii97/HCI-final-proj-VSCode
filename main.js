3c05730762123fb4630b27c0eb32e8114ad9b366
lukepotter <lpotter1@ithaca.edu>
2023-11-29 20:34:45 -0500

    the vehicle has entered the webpage

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+        // Add Ambient Light
+        const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+        scene.add(ambientLight);
 
-camera.position.z = 5;
+        const loader = new GLTFLoader();
+        loader.load('raw.glb', (gltf) => {
+            scene.add(gltf.scene);
+        }, undefined, (error) => {
+            console.error('Error loading GLB model:', error);
+        });
 
-function animate() {
-	requestAnimationFrame( animate );
+		const controls = new OrbitControls( camera, renderer.domElement );
+        camera.position.z = 5;
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
 
-	renderer.render( scene, camera );
-}
-
-animate();
+        animate();
