01b39f62443fd08f7b7da028894aa7e6524429b7
Jackson Gray <jgray6@ithaca.edu>
2023-12-05 13:40:25 -0500

    Fixed Camera angle

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 5, 10);
+camera.position.set(1, 1, -4);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
