0092393d3bf293ef2cd935a13a4830b60502eff0
lukepotter <lpotter1@ithaca.edu>
2023-12-05 19:17:54 -0500

    added code for garage without garage file

--- main.js
+++ main.js
@@ -1,77 +1,95 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
+
+let car; 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(1, 1, -4);
+camera.position.set(11, 2, 1);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 // Ambient Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
-
 
 // Directional Light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
 
+const mygarage = new GLTFLoader();
+mygarage.load('garage/uploads-files-4420114-new_garage_3D_scene.glb', (gltf) => {
+   const garage = gltf.scene;
+   scene.add(garage);
+   
+   const loader = new GLTFLoader();
+   loader.load('raw.glb', (carGltf) => {
+      car = carGltf.scene; 
+      car.position.set(5.7, 2.1, 0);
+      car.scale.set(1, 1, 1);
+      car.rotation.set(0, Math.PI / 2, 0);
 
-// Floor
-const floorGeometry = new THREE.PlaneGeometry(100, 100);
-const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x808080, metalness: 0.2, roughness: 0.8 });
-const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.rotation.x = -Math.PI / 2;
-floor.position.y = -2; // Adjust floor level here!
-scene.add(floor);
+      const horizontalRotationAngle = Math.PI / -1.5;
+      car.rotation.y += horizontalRotationAngle;
 
-
-// Loading 3D Model
-const loader = new GLTFLoader();
-loader.load('raw.glb', (gltf) => {
-   const car = gltf.scene;
-   car.position.y = 1;
-   scene.add(car);
+      scene.add(car);
+   }, undefined, (error) => {
+      console.error('Error loading car GLB model:', error);
+   });
 }, undefined, (error) => {
-   console.error('Error loading GLB model:', error);
+   console.error('Error loading garage model:', error);
+});
+
+let isDragging = false;
+let previousMouseX = 0;
+
+document.addEventListener('mousedown', (event) => {
+   isDragging = true;
+   previousMouseX = event.clientX;
+});
+
+document.addEventListener('mousemove', (event) => {
+   if (isDragging) {
+      const deltaMouseX = event.clientX - previousMouseX;
+      previousMouseX = event.clientX;
+
+      const rotationSpeed = 0.005;
+      car.rotation.y += deltaMouseX * rotationSpeed;
+   }
+});
+
+document.addEventListener('mouseup', () => {
+   isDragging = false;
 });
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
+controls.enabled = false;
 
-
-// Handle window resize, less laggings
 window.addEventListener('resize', () => {
    const newWidth = window.innerWidth;
    const newHeight = window.innerHeight;
 
-
    camera.aspect = newWidth / newHeight;
    camera.updateProjectionMatrix();
-
 
    renderer.setSize(newWidth, newHeight);
 });
 
-
-// Animation
 function animate() {
    requestAnimationFrame(animate);
 
-
-   controls.update();
-
-
    renderer.render(scene, camera);
 }
-
 
 animate();
 
