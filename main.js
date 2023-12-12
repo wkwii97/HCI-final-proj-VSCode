9e96094f53d4026b68b40705f9fbc2d8fd8939c9
askakov <askakov@ithaca.edu>
2023-12-12 13:13:44 -0500

    fixed sizes

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ mygarage.load('garage/uploads-files-4420114-new_garage_3D_scene.glb', (gltf) => 
       car = carGltf.scene; 
 
       /* Adjust these values to adjust car position..... (x, y, z) */
-      car.position.set(5.7, 2.1, 0);
+      car.position.set(5, 0, 0);
       car.scale.set(1, 1, 1);
       car.rotation.set(0, Math.PI / 2, 0);
 
